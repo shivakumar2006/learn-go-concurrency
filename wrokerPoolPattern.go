659da444417ea327fb4a8416c1264dbf4d697fb6
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-09 21:12:11 +0530

    comit

---
+++ wrokerPoolPattern.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	numJobs := 10
+	numWorkers := 3
+
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	var wg sync.WaitGroup
+
+	// start worker goroutine
+	for i := 1; i <= numWorkers; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			worker(workerID, jobs, results)
+		}(i)
+	}
+
+	// enqueue jobs
+	for i := 1; i <= numJobs; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	// wait for all waitgroup to complete the task
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// collect result
+	for result := range results {
+		fmt.Printf("results : %d\n", result)
+	}
+}
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for job := range jobs {
+		fmt.Printf("Worker %d procesing job %d\n", id, job)
+		results <- job * 2
+	}
+}
