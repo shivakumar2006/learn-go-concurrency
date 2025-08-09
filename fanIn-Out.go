069107778729aaeae271fe86d805c735e9b22c68
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-09 23:18:18 +0530

    commit

---
+++ fanIn-Out.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	data := []int{1, 2, 3, 4, 5}
+	input := make(chan int, len(data))
+	for _, d := range data {
+		input <- d
+	}
+	close(input)
+
+	// Fan-out launch multiple worjout goroutines
+	numWorkers := 3
+	results := make(chan int, len(data))
+
+	var wg sync.WaitGroup
+
+	for i := 0; i <= numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for num := range input {
+				result := num * 2
+				results <- result
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for result := range results {
+		fmt.Println(result)
+	}
+}
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	}(wg, mut)
 
 	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
-		fmt.Println("TWO R")
+		fmt.Println("Two R")
 		mut.Lock()
 		score = append(score, 2)
 		mut.Unlock()
@@ -33,7 +33,7 @@ func main() {
 	}(wg, mut)
 
 	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
-		fmt.Println("Three R")
+		fmt.Println("three R")
 		mut.Lock()
 		score = append(score, 3)
 		mut.Unlock()
@@ -42,9 +42,55 @@ func main() {
 
 	wg.Wait()
 
-	fmt.Println("All the goroutine fucntion runs successfully;")
+	fmt.Println("All the goroutines run successfully")
 	fmt.Println(score)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func main() {
+
+// 	wg := &sync.WaitGroup{}
+// 	mut := &sync.Mutex{}
+
+// 	score := []int{0}
+
+// 	wg.Add(3)
+
+// 	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+// 		fmt.Println("One R")
+// 		mut.Lock()
+// 		score = append(score, 1)
+// 		mut.Unlock()
+// 		wg.Done()
+// 	}(wg, mut)
+
+// 	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+// 		fmt.Println("TWO R")
+// 		mut.Lock()
+// 		score = append(score, 2)
+// 		mut.Unlock()
+// 		wg.Done()
+// 	}(wg, mut)
+
+// 	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+// 		fmt.Println("Three R")
+// 		mut.Lock()
+// 		score = append(score, 3)
+// 		mut.Unlock()
+// 		wg.Done()
+// 	}(wg, mut)
+
+// 	wg.Wait()
+
+// 	fmt.Println("All the goroutine fucntion runs successfully;")
+// 	fmt.Println(score)
+// }
 
 // waitgroup and mutex
 
