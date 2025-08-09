6019c62ed8268850f04436a657dc58758b49262d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-09 23:52:47 +0530

    comit

--- fanIn-Out.go
+++ fanIn-Out.go
@@ -13,7 +13,6 @@ func main() {
 	}
 	close(input)
 
-	// Fan-out launch multiple worjout goroutines
 	numWorkers := 3
 	results := make(chan int, len(data))
 
@@ -24,8 +23,7 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for num := range input {
-				result := num * 2
-				results <- result
+				results <- num * 2
 			}
 		}()
 	}
@@ -39,3 +37,45 @@ func main() {
 		fmt.Println(result)
 	}
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func main() {
+// 	data := []int{1, 2, 3, 4, 5}
+// 	input := make(chan int, len(data))
+// 	for _, d := range data {
+// 		input <- d
+// 	}
+// 	close(input)
+
+// 	// Fan-out launch multiple workout goroutines
+// 	numWorkers := 3
+// 	results := make(chan int, len(data))
+
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i <= numWorkers; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+// 			for num := range input {
+// 				result := num * 2
+// 				results <- result
+// 			}
+// 		}()
+// 	}
+
+// 	go func() {
+// 		wg.Wait()
+// 		close(results)
+// 	}()
+
+// 	for result := range results {
+// 		fmt.Println(result)
+// 	}
+// }
