4a33fe55929e29c4f97336d7efed89a9ae097d31
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-10 14:56:01 +0530

    commit

--- concurrency /fanIn-Out.go
+++ concurrency /fanIn-Out.go
@@ -53,6 +53,46 @@ func main() {
 // 	}
 // 	close(input)
 
+// 	numWorkers := 3
+// 	results := make(chan int, len(data))
+
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i <= numWorkers; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+// 			for num := range input {
+// 				results <- num * 2
+// 			}
+// 		}()
+// 	}
+
+// 	go func() {
+// 		wg.Wait()
+// 		close(results)
+// 	}()
+
+// 	for result := range results {
+// 		fmt.Println(result)
+// 	}
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func main() {
+// 	data := []int{1, 2, 3, 4, 5}
+// 	input := make(chan int, len(data))
+// 	for _, d := range data {
+// 		input <- d
+// 	}
+// 	close(input)
+
 // 	// Fan-out launch multiple workout goroutines
 // 	numWorkers := 3
 // 	results := make(chan int, len(data))
