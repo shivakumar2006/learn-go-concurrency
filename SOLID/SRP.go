8ad411cec310be33668d63007f62c531d64d4687
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-10 15:23:11 +0530

    commit

---
+++ SOLID/SRP.go
@@ -0,0 +1,66 @@
+// // SOLID
+
+// // S := Single Responsibility Principal
+
+package main
+
+import (
+	"fmt"
+)
+
+// This is bad typo because we need to make functions like that only work for one function
+type userManager struct{}
+
+func (u *userManager) SaveUser(name string) {
+	fmt.Println("Save user : ", name)
+}
+
+func (u *userManager) SendEmail(email, message string) {
+	fmt.Println("Send email : ", email)
+}
+
+type EmailSender struct{}
+
+func (e *EmailSender) Send(email, message string) {
+	fmt.Println("Send email : ", email)
+}
+
+func main() {
+	userMgr := &userManager{}
+	userMgr.SaveUser("Shiva")
+
+	emailSender := &EmailSender{}
+	emailSender.Send("Shiva@gmail.com", "Welcome!")
+}
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // Bad : user manager does two things user data and email sending
+// type userManager struct{}
+
+// func (u *userManager) SaveUser(name string) {
+// 	fmt.Println("user saved : ", name)
+// }
+
+// func (u *userManager) SendEmail(email, message string) {
+// 	fmt.Println("Email sent to : ", email)
+// }
+
+// // Good seperate Email sender
+// type EmailSender struct{}
+
+// func (e *EmailSender) Send(email, message string) {
+// 	fmt.Println("Email sent to : ", email)
+// }
+
+// func main() {
+// 	userMgr := &userManager{}
+// 	userMgr.SaveUser("Alice")
+
+// 	emailSender := &EmailSender{}
+// 	emailSender.Send("alice@gmail.com", "Welcome!")
+// }
