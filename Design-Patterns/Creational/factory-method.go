b91681b2f8b0cbd2e438f45544b5b5f63425cf15
shivakumar2006 <official.shivakumar06@gmail.com>
2025-11-21 16:29:33 +0530

    commit

--- Design-Patterns/Creational/factory-method.go
+++ Design-Patterns/Creational/factory-method.go
@@ -1,52 +1,141 @@
-//factory method pattern
+// //factory method pattern
 
 package main
 
 import "fmt"
 
-// product interface
-type IceCream interface {
-	Flavour() string
+// profuct interface
+type IGun interface {
+	setName(name string)
+	setPower(power int)
+	getName() string
+	getPower() int
 }
 
-// concrete icecream products
-type Choclate struct{}
-
-func (c Choclate) Flavour() string {
-	return "Choclate Ice-cream"
+// concrete products
+type Gun struct {
+	name  string
+	power int
 }
 
-type Vanilla struct{}
-
-func (v Vanilla) Flavour() string {
-	return "Vanilla ice cream"
+func (g *Gun) setName(name string) {
+	g.name = name
 }
 
-type Strawberry struct{}
-
-func (s Strawberry) Flavour() string {
-	return "Strawberry ice cream"
+func (g *Gun) getName() string {
+	return g.name
 }
 
-// factory method
-func IceCreamFactory(choice string) IceCream {
-	switch choice {
-	case "choclate":
-		return Choclate{}
-	case "vanilla":
-		return Vanilla{}
-	case "strawberry":
-		return Strawberry{}
-	default:
-		return nil
+func (g *Gun) setPower(power int) {
+	g.power = power
+}
+
+func (g *Gun) getPower() int {
+	return g.power
+}
+
+// concrete product
+type AK47 struct {
+	Gun
+}
+
+func newAk47() IGun {
+	return &AK47{
+		Gun: Gun{
+			name:  "AK47 gun",
+			power: 4,
+		},
 	}
 }
 
-// client (customer)
-func main() {
-	ice1 := IceCreamFactory("choclate")
-	ice2 := IceCreamFactory("strawberry")
-
-	fmt.Println("Customer 1 got: ", ice1.Flavour())
-	fmt.Println("Customer 2 got: ", ice2.Flavour())
+// concrete product
+type Musket struct {
+	Gun
 }
+
+func newMusket() IGun {
+	return &Musket{
+		Gun: Gun{
+			name:  "Musket gun",
+			power: 1,
+		},
+	}
+}
+
+// factory
+func getGun(gunType string) (IGun, error) {
+	if gunType == "ak47" {
+		return newAk47(), nil
+	}
+	if gunType == "musket" {
+		return newMusket(), nil
+	}
+	return nil, fmt.Errorf("Wrong gun type passed")
+}
+
+// client code
+func main() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun : %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Gun : %d", g.getPower())
+	fmt.Println()
+}
+
+// package main
+
+// import "fmt"
+
+// // product interface
+// type IceCream interface {
+// 	Flavour() string
+// }
+
+// // concrete icecream products
+// type Choclate struct{}
+
+// func (c Choclate) Flavour() string {
+// 	return "Choclate Ice-cream"
+// }
+
+// type Vanilla struct{}
+
+// func (v Vanilla) Flavour() string {
+// 	return "Vanilla ice cream"
+// }
+
+// type Strawberry struct{}
+
+// func (s Strawberry) Flavour() string {
+// 	return "Strawberry ice cream"
+// }
+
+// // factory method
+// func IceCreamFactory(choice string) IceCream {
+// 	switch choice {
+// 	case "choclate":
+// 		return Choclate{}
+// 	case "vanilla":
+// 		return Vanilla{}
+// 	case "strawberry":
+// 		return Strawberry{}
+// 	default:
+// 		return nil
+// 	}
+// }
+
+// // client (customer)
+// func main() {
+// 	ice1 := IceCreamFactory("choclate")
+// 	ice2 := IceCreamFactory("strawberry")
+
+// 	fmt.Println("Customer 1 got: ", ice1.Flavour())
+// 	fmt.Println("Customer 2 got: ", ice2.Flavour())
+// }
