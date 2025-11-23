e3b7c331d37d5330f678c73ff6f0bf1b3aebaf09
shivakumar2006 <official.shivakumar06@gmail.com>
2025-11-23 09:29:06 +0530

    bridge complete

---
+++ Design-Patterns/Structural/Bridge.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+// abstraction
+type Computer interface {
+	Print()
+	SetPrinter(Printer)
+}
+
+type Printer interface {
+	PrintFile()
+}
+
+// refined abstraction
+type Mac struct {
+	printer Printer
+}
+
+func (m *Mac) Print() {
+	fmt.Println("Print request for mac")
+	m.printer.PrintFile()
+}
+
+func (m *Mac) SetPrinter(p Printer) {
+	m.printer = p
+}
+
+type Windows struct {
+	printer Printer
+}
+
+func (w *Windows) Print() {
+	fmt.Println("Print request for windows")
+	w.printer.PrintFile()
+}
+
+func (w *Windows) SetPrinter(p Printer) {
+	w.printer = p
+}
+
+type Epson struct{}
+
+func (p *Epson) PrintFile() {
+	fmt.Println("Printing by a EPSON printer")
+}
+
+type Hp struct{}
+
+func (h *Hp) PrintFile() {
+	fmt.Println("Printing by a HP printer")
+}
+
+func main() {
+	hpPrinter := &Hp{}
+	epsonPrinter := &Epson{}
+
+	macComputer := &Mac{}
+
+	macComputer.SetPrinter(hpPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	macComputer.SetPrinter(epsonPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	winComputer := &Windows{}
+
+	winComputer.SetPrinter(hpPrinter)
+	winComputer.Print()
+	fmt.Println()
+
+	winComputer.SetPrinter(epsonPrinter)
+	winComputer.Print()
+	fmt.Println()
+}
