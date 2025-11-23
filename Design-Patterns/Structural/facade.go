33edc5d01b918dbef8dd7e7eb76a5af97d9c4c16
shivakumar2006 <official.shivakumar06@gmail.com>
2025-11-23 12:08:59 +0530

    commit

---
+++ Design-Patterns/Structural/facade.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type WalletFacade struct {
+	account      *Account
+	wallet       *Wallet
+	securityCode *SecurityCode
+	notification *Notification
+	ledger       *Ledger
+}
+
+func newWalletFacade(accountID string, code int) *WalletFacade {
+	fmt.Println("Started creating account")
+	walletFacade := &WalletFacade{
+		account:      newAccount(accountID),
+		securityCode: newSecurityCode(code),
+		wallet:       newWallet(),
+		notification: &Notification{},
+		ledger:       &Ledger{},
+	}
+	fmt.Println("Account created")
+	return walletFacade
+}
+
+func (w *WalletFacade) addMoneyToWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting add money to wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	w.wallet.creditBalance(amount)
+	w.notification.sendWalletCreditNotification()
+	w.ledger.makeEntry(accountID, "credit", amount)
+	return nil
+}
+
+func (w *WalletFacade) deductMoneyFromWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting debit money from wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	err = w.wallet.debitBalance(amount)
+	if err != nil {
+		return err
+	}
+	w.notification.sendWalletDebitNotification()
+	w.ledger.makeEntry(accountID, "debit", amount)
+	return nil
+}
+
+// account : complex subsystem parts
+type Account struct {
+	name string
+}
+
+func newAccount(accountName string) *Account {
+	return &Account{
+		name: accountName,
+	}
+}
+
+func (a *Account) checkAccount(accountName string) error {
+	if a.name != accountName {
+		return fmt.Errorf("Account name is incorrect")
+	}
+	fmt.Println("Account verified")
+	return nil
+}
+
+// security code
+type SecurityCode struct {
+	code int
+}
+
+func newSecurityCode(code int) *SecurityCode {
+	return &SecurityCode{
+		code: code,
+	}
+}
+
+func (s *SecurityCode) checkCode(incomingCode int) error {
+	if s.code != incomingCode {
+		return fmt.Errorf("Security code is incorrect")
+	}
+	fmt.Println("Security code verified")
+	return nil
+}
+
+// wallet
+type Wallet struct {
+	balance int
+}
+
+func newWallet() *Wallet {
+	return &Wallet{
+		balance: 0,
+	}
+}
+
+func (w *Wallet) creditBalance(amount int) {
+	w.balance += amount
+	fmt.Println("Wallet balance added successfully!")
+	return
+}
+
+func (w *Wallet) debitBalance(amount int) error {
+	if w.balance < amount {
+		return fmt.Errorf("Balance is not sufficient!")
+	}
+	fmt.Println("Wallet balance is sufficient")
+	w.balance -= amount
+	return nil
+}
+
+// ledger
+type Ledger struct{}
+
+func (s *Ledger) makeEntry(accountID, txnType string, amount int) {
+	fmt.Printf("Make ledger entry for account id %s with txnType %s for amount %d\n", accountID, txnType, amount)
+	return
+}
+
+// notification
+type Notification struct{}
+
+func (n *Notification) sendWalletCreditNotification() {
+	fmt.Println("Sending wallet credit notification")
+}
+
+func (n *Notification) sendWalletDebitNotification() {
+	fmt.Println("Sending walled debit notification")
+}
+
+func main() {
+	fmt.Println()
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatal("Error : %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatal("Error : %s\n", err.Error())
+	}
+}
