e04d17a07fe961a367fc504ef87a8b7ac845aa62
shivakumar2006 <official.shivakumar06@gmail.com>
2026-06-06 23:27:12 +0530

    feat: initialize project structure with configuration loader and dependency management

---
+++ students-api/internal/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+
+type Config struct {
+	Env         string `yaml:"env" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var ConfigPath string
+
+	ConfigPath = os.Getenv("CONFIG_PATH")
+
+	if ConfigPath == "" {
+		flags := flag.String("config", "", "Path to config file")
+		flag.Parse()
+
+		ConfigPath = *flags
+
+		if ConfigPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(ConfigPath); os.IsNotExist(err) {
+		log.Fatalf("Config file doesn't exist %s", ConfigPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(ConfigPath, &cfg)
+	if err != nil {
+		log.Fatalf("Cannot read config file %s", err.Error())
+	}
+
+	return &cfg
+}
