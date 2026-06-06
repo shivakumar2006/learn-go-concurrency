a2827302fd6d9962de5384ce57105b6fffa2c9dd
shivakumar2006 <official.shivakumar06@gmail.com>
2026-06-07 00:06:04 +0530

    feat: implement HTTP server with graceful shutdown and add configuration tags

--- students-api/cmd/main.go
+++ students-api/cmd/main.go
@@ -1,5 +1,61 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"students-api/internal/config"
+	"syscall"
+	"time"
+)
 
+func main() {
+	// load config
+	cfg := config.MustLoad()
+
+	// setup db
+	// add router
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	// gracefully shutdown
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server successfully stopped")
 }
